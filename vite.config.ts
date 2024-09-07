e760540b90d6ab13fc59c5fa8635cb73f883e9dd
Yondo202 <yondooo33@gmail.com>
2024-09-07 22:26:30 +0800

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import vercel from 'vite-plugin-vercel';
 import { defineConfig } from "vite"
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
